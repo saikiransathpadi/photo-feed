e5efa682e853669df86b7f9073ab796ae8c3ac59
SaiKiranS <sathpadisaikiran@gmail.com>
2021-03-07 19:24:09 +0530

    fetched images from unsplash

--- src/photofeed/base/basecomp.js
+++ src/photofeed/base/basecomp.js
@@ -1,10 +1,11 @@
 import './basecomp.css'
-
+import Imagefeed from '../imagefeed/imagefeed'
 export default function Base(){
     return (
         <div>
             <body>
                 <h1 className='head'>Photo Feed</h1>
+                <Imagefeed/>
             </body>
         </div>
     )
--- src/photofeed/imagefeed/imagefeed.js
+++ src/photofeed/imagefeed/imagefeed.js
@@ -1,11 +1,37 @@
 import {useState,useEffect} from 'react'
-
+import axios from 'axios';
+import './image.css'
 export default function Imagefeed(){
-    const [images, setimages] = usestate([])
+    const [images, setimages] = useState([])
+
+    const Eachimageelement = ({ url, key }) => (
+        <div className="image-item" key={key} >
+          <img src={url} />
+        </div>
+      );
+
+    const fetchImages = (count = 10) => {
+        const accessKey = "6lw_er5SLMrXlcDbJVfGg1d_zxLKqgdfYYfkSljUNpA";
     
+        axios
+          .get(`https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${count}`)
+          .then (res => {
+            setimages([...images, ...res.data]);
+          });
+    };
+    useEffect(()=>{
+        fetchImages()
+        console.log(images)
+    },[])
     return (
         <div>
-
+            {images.map((image, index) => (
+                    <Eachimageelement
+                      url={image.urls.regular}
+                      key={index}
+                    />
+                  ))
+                }
         </div>
     )
 }
